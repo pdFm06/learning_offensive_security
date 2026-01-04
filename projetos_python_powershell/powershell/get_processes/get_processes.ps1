4c9deaad9c51ffc180ad98f3d79d55539878d3fc
pdfms <pdfms>
2026-01-04 16:13:54 +0000

    Colocar o get_processes.ps1

---
+++ projetos_python_powershell/powershell/get_processes/get_processes.ps1
@@ -0,0 +1,2 @@
+#Script simples que exporta todos os processos em execução para um ficheiro csv.
+Get-Process | Select-Object -Property ProcessName, Id, SI | Export-Csv -Path .\processos.csv -NoTypeInformation
