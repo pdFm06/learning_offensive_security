9cd3839ec1f8dcaa475660e3b5a6bf21046c4069
pdFm06 <pedrodiogomatos4@gmail.com>
2026-02-06 16:49:29 +0000

    Inserir a nova ferramenta - webenum.py

---
+++ tools/web_enum/README.md
@@ -0,0 +1,36 @@
+# WebEnum
+
+Basic tool for automated initial web reconnaissance using:
+- Nmap (NSE)
+- Gobuster (vhost)
+- Dirsearch
+- WhatWeb
+
+Designed for CTFs, labs and authorized security assessments.
+
+## Requirements
+
+- Python 3.9+
+- nmap
+- gobuster
+- dirsearch
+- whatweb
+- SecLists
+
+## Installation
+
+sudo apt install nmap gobuster dirsearch whatweb seclists
+git clone 
+
+## Usage
+
+sudo python3 webenum.py -u cap.htb -p 80 -w wordlist.txt
+
+With options:
+
+--no-gobuster
+--no-dirsearch
+--no-whatweb
+--verbose
+
+Some Nmap scans may require elevated privileges.
--- tools/web_enum/webenum.py
+++ tools/web_enum/webenum.py
@@ -1,54 +1,225 @@
-# Objetivo do script: integrar num só comando 4 ferramentas/analises de uma webapp.
-# 1º - Nmap NSE
-# 2º - Vhost enum
-# 3º - Dirbusting
-# 4º - WhatWeb
-
-# 1º Passo: Receber um IP ou domínio e verificar se ele é válido
-# Estou a pensar em usar Regex para isto
-# Após falar com o Chat, seria ideal usar Regex para domínios e a bilbioteca ipaddress para o ip.
-
-# Bibliotecas necessárias
 import ipaddress
 import re
 import socket
+import subprocess
+import os
+import argparse
 
-target = input("Target: ")
-target.strip()
-target_type = None
+parser = argparse.ArgumentParser(
+    description="Executes Nmap NSE + Gobuster (Vhost fuzzing) + Dirsearch (Dirbrute) + Whatweb (fingerprinting) in one script"
+)
 
-domain_pattern = r"^(?=.{1,253}$)([a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z]{2,63}$" 
-domain_regex = re.compile(domain_pattern, re.IGNORECASE)
+parser.add_argument(
+    "-u", "--target",
+    required=True,
+    help="Target's IP or domain"
+)
 
-def is_ip(target):
+parser.add_argument(
+    "-p", "--port",
+    type=int,
+    default=80,
+    help="HTTP port (default: 80)"
+)
+
+parser.add_argument(
+    "-w", "--wordlist",
+    required=True,
+    help="Gobuster wordlist (DNS)"
+)
+
+parser.add_argument(
+    "-v", "--verbose",
+    action="store_true",
+    help="Enable verbose output"
+)
+
+parser.add_argument(
+    "--no-nmap",
+    action="store_true",
+    help="Skip Nmap"
+)
+
+parser.add_argument(
+    "--no-gobuster",
+    action="store_true",
+    help="Skip Gobuster Vhost fuzzing"
+)
+
+parser.add_argument(
+    "--no-dirsearch",
+    action="store_true",
+    help="Skip Dirsearch bruteforcing"
+)
+
+parser.add_argument(
+    "--no-whatweb",
+    action="store_true",
+    help="Skip WhatWeb fingerprinting"
+)
+
+parser.add_argument(
+    "-t", "--threads",
+    required=False,
+    type=int,
+    default=20,
+    help="Threads for bruteforce tools (default: 20)"
+)
+
+args = parser.parse_args()
+target = args.target
+port = args.port
+wordlist = args.wordlist
+verbose = args.verbose
+threads = args.threads
+
+
+def ensure_outputs_dir(ip):
+    os.makedirs(f"{ip}_outputs", exist_ok=True)
+
+
+def run_nmap(target, port, timeout=600):
+    scripts = "http-enum,http-title,http-headers"
+
+    cmd = [
+        "nmap",
+        "-sV",
+        "-p", str(port),
+        "--script", scripts,
+    ]
+
+    if verbose:
+        cmd += [
+                "-vvv",
+                "--stats-every=10s" 
+        ]
+        print("\n[+] Running Nmap...\n")
+    cmd += [target, "-oA", os.path.join(f"{target}_outputs", f"{target}_nmapScan")]
+
+    
+    p = subprocess.run(cmd, text=True, timeout=timeout)
+    return {"cmd": cmd, "returncode": p.returncode}
+
+
+def run_gobuster(target, domain, wordlist, threads, port, timeout=600):
+    url = f"http://{target}:{port}/"
+
+    cmd = [
+            "gobuster", "vhost",
+            "-u", url,
+            "-w", wordlist,
+            "-t", str(threads),
+            "--output", os.path.join(f"{target}_outputs", f"{target}_vhostFuzzing.txt"),
+        ]
+    
+    if not verbose:
+        cmd += ["--np"]
+
+    if domain:
+        cmd += ["--domain", domain, "--append-domain"]
+    else:
+        print("[!] No domain...")
+
+    if verbose:
+        print("\n[+] Running Gobuster...\n")
+    p = subprocess.run(cmd, text=True, timeout=timeout)
+    return {"cmd": cmd, "returncode": p.returncode}
+
+
+def run_dirsearch(target, threads, port, timeout=600):
+    url = f"http://{target}:{port}/"
+
+    cmd = [
+        "dirsearch",
+        "-u", url,
+        "-t", str(threads),
+        "--output", os.path.join(f"{target}_outputs", f"{target}_dirsearchFuzzing.txt")
+    ]
+
+    if verbose:
+        print("\n[+] Running Dirsearch...\n")
+    p = subprocess.run(cmd, text=True, timeout=timeout)
+    return {"cmd": cmd, "returncode": p.returncode}
+
+
+def run_whatweb(target, port, timeout=600):
+    url = f"http://{target}:{port}"
+
+    cmd = [
+        "whatweb",
+        "-v",
+        url,
+        "--log-verbose", os.path.join(f"{target}_outputs", f"{target}_whatwebAnalysis.txt")
+    ]
+
+    if verbose:
+        print("\n[+] Running Whatweb...\n")
+    p = subprocess.run(cmd, text=True, timeout=timeout)
+    return {"cmd": cmd, "returncode": p.returncode}
+
+
+def is_ip(value):
     try:
-        ipaddress.ip_address(target)
+        ipaddress.ip_address(value)
         return True
     except ValueError:
         return False
 
 
-# Verificar se é um IP ou se é um domínio
-if is_ip(target):
-    target_type = "IP"
-
-elif domain_regex.fullmatch(target):
-    target_type = "Domain"
-
-else:
-    raise ValueError("Invalid target")
-
-def resolve_domain_to_ips(host: str) -> list[str]: # host: str -> list[str] são type hints: neste caso, esperamos receber uma string e devolver uma lista de strings
-    infos = socket.getaddrinfo(host, None) # Resolve o nome do domínio com nenhuma porta especificada e devolve uma lista de registos com informações da rede, nomeadamente: family, socktype, protocol, cannonname, sockaddr
-
-    unique_ips = set() #Criar um set. Um set não tem duplicados e não garante ordem
+def resolve_domain_to_ips(host):
+    infos = socket.getaddrinfo(host, None)
+    ip_set = set()
     for family, socktype, proto, canonname, sockaddr in infos:
-        ip = sockaddr[0] # O sockaddr tem o ip[0] e a porta[0]. Só queremos o IP.
-        unique_ips.add(ip)
+        ip_set.add(sockaddr[0])
+    return sorted(ip_set)
 
-    return sorted(unique_ips)
 
-if target_type == "Domain":
-    ips = resolve_domain_to_ips(target)
-    print(ips)
+def main():
+    domain_pattern = r"^(?=.{1,253}$)([a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z]{2,63}$"
+    domain_regex = re.compile(domain_pattern, re.IGNORECASE)
 
+    domain = None
+    ip = None
+
+    if is_ip(target):
+        ip = target
+    elif domain_regex.fullmatch(target):
+        domain = target.lower().strip()
+        ips = resolve_domain_to_ips(domain)
+        if not ips:
+            raise ValueError("Could not resolve domain")
+        ip = ips[0]
+    else:
+        raise ValueError("Invalid target")
+
+    ensure_outputs_dir(ip)
+
+    if not os.path.isfile(wordlist):
+        raise FileNotFoundError(f"{wordlist} not found!")
+
+    if not args.no_nmap:
+        resNmap = run_nmap(ip, port=port, timeout=600)
+        if resNmap["returncode"] != 0:
+            print("[!] Nmap error!")
+
+    if not args.no_gobuster:
+        if domain is None:
+            print("[!] No domain: skipping Vhost fuzzing...")
+        else:
+            resGobuster = run_gobuster(ip, domain, wordlist, threads=threads, port=port, timeout=600)
+            if resGobuster["returncode"] != 0:
+                print("[!] Gobuster error!")
+
+    if not args.no_dirsearch:
+        resDirSearch = run_dirsearch(ip, threads=threads, port=port, timeout=600)
+        if resDirSearch["returncode"] != 0:
+            print("[!] Dirsearch error!")
+
+    if not args.no_whatweb:
+        resWhatWeb = run_whatweb(ip, port=port, timeout=600)
+        if resWhatWeb["returncode"] != 0:
+            print("[!] Whatweb error!")
+
+
+if __name__ == "__main__":
+    main()
